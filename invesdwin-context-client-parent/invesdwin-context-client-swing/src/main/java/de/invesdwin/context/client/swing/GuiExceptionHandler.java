8ccc2fc471ea1ec67f311e1011a154b4f067d3ac
subes <gsubes@gmail.com>
2019-02-16 14:47:10 +0100

    dont show errors on shutdown

--- invesdwin-context-client-parent/invesdwin-context-client-swing/src/main/java/de/invesdwin/context/client/swing/GuiExceptionHandler.java
+++ invesdwin-context-client-parent/invesdwin-context-client-swing/src/main/java/de/invesdwin/context/client/swing/GuiExceptionHandler.java
@@ -20,6 +20,7 @@ import de.invesdwin.context.log.error.LoggedRuntimeException;
 import de.invesdwin.context.log.error.hook.ErrHookManager;
 import de.invesdwin.context.log.error.hook.IErrHook;
 import de.invesdwin.util.assertions.Assertions;
+import de.invesdwin.util.shutdown.ShutdownHookManager;
 
 @ThreadSafe
 public final class GuiExceptionHandler implements IErrHook {
@@ -44,7 +45,7 @@ public final class GuiExceptionHandler implements IErrHook {
 
     @Override
     public void loggedException(final LoggedRuntimeException e, final boolean uncaughtException) {
-        if (uncaughtException) {
+        if (uncaughtException && !ShutdownHookManager.isShuttingDown()) {
             for (final IGuiExceptionHandlerHook hook : hooks) {
                 if (hook.shouldHideException(e)) {
                     return;
