3453cda414167397d827d89a595541daefb1d4e2
subes <gsubes@gmail.com>
2019-02-18 13:17:52 +0100

    exportIcons

---
+++ invesdwin-context-client-parent/invesdwin-context-client-swing/src/test/java/de/invesdwin/context/client/swing/ExportIcons.java
@@ -0,0 +1,45 @@
+package de.invesdwin.context.client.swing;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import javax.imageio.ImageIO;
+
+import org.kordamp.ikonli.fontawesome5.FontAwesomeSolid;
+import org.kordamp.ikonli.swing.FontIcon;
+
+import de.invesdwin.context.ContextProperties;
+
+@NotThreadSafe
+public final class ExportIcons {
+
+    private ExportIcons() {}
+
+    //CHECKSTYLE:OFF
+    public static void main(final String[] args) throws IOException {
+        //CHECKSTYLE:ON
+        exportIcon(newIcon(FontAwesomeSolid.TRASH_ALT), "trash");
+        exportIcon(newIcon(FontAwesomeSolid.PLUS), "add");
+        exportIcon(newIcon(FontAwesomeSolid.TIMES), "delete");
+
+    }
+
+    private static FontIcon newIcon(final FontAwesomeSolid iconType) {
+        return FontIcon.of(iconType, 256, new Color(20, 20, 20));
+    }
+
+    private static void exportIcon(final FontIcon icon, final String name) throws IOException {
+        final BufferedImage bi = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(),
+                BufferedImage.TYPE_4BYTE_ABGR);
+
+        final Graphics2D g2 = bi.createGraphics();
+        icon.paintIcon(null, g2, 0, 0);
+        g2.dispose();
+        ImageIO.write(bi, "png", new File(ContextProperties.getCacheDirectory(), name + ".png"));
+    }
+
+}
