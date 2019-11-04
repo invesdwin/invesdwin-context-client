6e5af00f81c25a64cdaec932adf843b6b3c0eeb1
subes <gsubes@gmail.com>
2019-11-04 14:57:21 +0100

    disable retry in webserver

--- invesdwin-context-client-parent/invesdwin-context-client-wicket/src/main/java/de/invesdwin/context/client/wicket/internal/DelegateWicketFilter.java
+++ invesdwin-context-client-parent/invesdwin-context-client-wicket/src/main/java/de/invesdwin/context/client/wicket/internal/DelegateWicketFilter.java
@@ -1,7 +1,17 @@
 package de.invesdwin.context.client.wicket.internal;
 
-import javax.annotation.concurrent.NotThreadSafe;
+import java.io.IOException;
 
+import javax.annotation.concurrent.NotThreadSafe;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.http.WebResponse;
+
+import de.invesdwin.context.integration.IntegrationProperties;
 import de.invesdwin.nowicket.application.IWebApplicationConfig;
 import de.invesdwin.nowicket.application.filter.AWicketFilter;
 
@@ -11,6 +21,18 @@ public class DelegateWicketFilter extends AWicketFilter {
     @Override
     protected IWebApplicationConfig newConfig() {
         return DelegateWebApplication.staticResolveDelegate();
+    }
+
+    @Override
+    protected boolean processRequestCycle(final RequestCycle requestCycle, final WebResponse webResponse,
+            final HttpServletRequest httpServletRequest, final HttpServletResponse httpServletResponse,
+            final FilterChain chain) throws IOException, ServletException {
+        final boolean registerRetryDisabled = IntegrationProperties.registerThreadRetryDisabled();
+        try {
+            return super.processRequestCycle(requestCycle, webResponse, httpServletRequest, httpServletResponse, chain);
+        } finally {
+            IntegrationProperties.unregisterThreadRetryDisabled(registerRetryDisabled);
+        }
     }
 
 }
