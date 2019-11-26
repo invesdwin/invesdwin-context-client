bcd4846501b3558abe058947bc1fdbfbf832fdf8
subes <gsubes@gmail.com>
2019-11-26 23:11:39 +0100

    trim earlier

--- invesdwin-context-client-parent/invesdwin-context-client-swing-jfreechart/src/main/java/de/invesdwin/context/client/swing/jfreechart/plot/dataset/list/MasterLazyDatasetList.java
+++ invesdwin-context-client-parent/invesdwin-context-client-swing-jfreechart/src/main/java/de/invesdwin/context/client/swing/jfreechart/plot/dataset/list/MasterLazyDatasetList.java
@@ -30,14 +30,13 @@ public class MasterLazyDatasetList extends ALazyDatasetList<OHLCDataItem> implem
 
     public static final OHLCDataItem DUMMY_VALUE = new OHLCDataItem(FDate.MIN_DATE.dateValue(), Double.NaN, Double.NaN,
             Double.NaN, Double.NaN, Double.NaN);
+    private static final int PRELOAD_RANGE_MULTIPLIER = 2;
     /*
      * keep a few more items because we need to keep some buffer to the left and right and we don't want to load data on
      * all the smaller panning actions
      */
-    private static final int STEP_ITEM_COUNT = PlotZoomHelper.MAX_ZOOM_ITEM_COUNT;
-    private static final int INITIAL_ITEM_COUNT = PlotZoomHelper.MAX_ZOOM_ITEM_COUNT * 2;
-    private static final int MAX_ITEM_COUNT = STEP_ITEM_COUNT * 5;
-    private static final int TRIM_ITEM_COUNT = MAX_ITEM_COUNT * 2;
+    private static final int MAX_ITEM_COUNT = PlotZoomHelper.MAX_ZOOM_ITEM_COUNT * 5;
+    private static final int TRIM_ITEM_COUNT = PlotZoomHelper.MAX_ZOOM_ITEM_COUNT * 7;
     private final IMasterLazyDatasetProvider provider;
     private final Set<ISlaveLazyDatasetListener> slaveDatasetListeners;
     private final Set<IRangeListener> rangeListeners;
@@ -129,8 +128,9 @@ public class MasterLazyDatasetList extends ALazyDatasetList<OHLCDataItem> implem
     }
 
     private void loadInitialDataMaster() {
+        final int initialVisibleItemCount = chartPanel.getInitialVisibleItemCount() * PRELOAD_RANGE_MULTIPLIER;
         final ICloseableIterable<? extends OHLCDataItem> initialValues = provider
-                .getPreviousValues(provider.getLastAvailableKey(), INITIAL_ITEM_COUNT);
+                .getPreviousValues(provider.getLastAvailableKey(), initialVisibleItemCount);
         final List<OHLCDataItem> data = getData();
         try (ICloseableIterator<? extends OHLCDataItem> it = initialValues.iterator()) {
             while (true) {
@@ -187,27 +187,28 @@ public class MasterLazyDatasetList extends ALazyDatasetList<OHLCDataItem> implem
         final boolean isTrailing = isTrailing(range);
         Range updatedRange = range;
         final int preloadLowerBound = (int) (range.getLowerBound() - range.getLength());
+        final List<OHLCDataItem> data = getData();
         if (preloadLowerBound < 0) {
             final FDate firstLoadedKey = getFirstLoadedKey();
             if (firstAvailableKey.isBefore(firstLoadedKey)) {
                 //prepend a whole screen additional to the requested items
-                int prependCount = STEP_ITEM_COUNT;
+                int prependCount = Integers.abs(preloadLowerBound);
                 prependCount = prependMaster(prependCount, firstLoadedKey);
-                System.out.println("prepend " + prependCount + " => " + getData().size());
+                System.out.println("prepend " + prependCount + " => " + data.size());
                 prependSlaves(prependCount);
                 updatedRange = new Range(range.getLowerBound() + prependCount, range.getUpperBound() + prependCount);
                 rangeChanged.setTrue();
             }
         }
         final int preloadUpperBound = (int) (range.getUpperBound() + range.getLength());
-        if (preloadUpperBound > getData().size()) {
+        if (preloadUpperBound > data.size()) {
             final FDate lastLoadedKey = getLastLoadedKey();
             if (provider.getLastAvailableKey().isAfter(lastLoadedKey)) {
                 //append a whole screen additional to the requested items
-                int appendCount = STEP_ITEM_COUNT;
+                int appendCount = preloadUpperBound - data.size();
                 if (appendCount > 0) {
                     appendCount = appendMaster(appendCount);
-                    System.out.println("append " + appendCount + " => " + getData().size());
+                    System.out.println("append " + appendCount + " => " + data.size());
                     appendSlaves(appendCount);
                     if (isTrailing) {
                         updatedRange = new Range(range.getLowerBound() + appendCount,
