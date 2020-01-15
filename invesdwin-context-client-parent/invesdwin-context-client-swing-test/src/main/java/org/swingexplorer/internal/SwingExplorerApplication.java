31bdc2a768b10fa761fabeeebed85ddc1c1f538d
subes <gsubes@gmail.com>
2020-01-15 13:04:25 +0100

    deadlock fix on shutdown

--- invesdwin-context-client-parent/invesdwin-context-client-swing-test/src/main/java/org/swingexplorer/internal/SwingExplorerApplication.java
+++ invesdwin-context-client-parent/invesdwin-context-client-swing-test/src/main/java/org/swingexplorer/internal/SwingExplorerApplication.java
@@ -3,6 +3,7 @@ package org.swingexplorer.internal;
 import java.io.Closeable;
 
 import javax.annotation.concurrent.NotThreadSafe;
+import javax.swing.JFrame;
 
 @NotThreadSafe
 public class SwingExplorerApplication extends Application implements Closeable {
@@ -10,7 +11,14 @@ public class SwingExplorerApplication extends Application implements Closeable {
     @Override
     public void close() {
         if (frmMain != null) {
-            frmMain.dispose();
+            final JFrame frmMainCopy = frmMain;
+            new Thread(SwingExplorerApplication.class.getSimpleName() + "_DISPOSE") {
+                @Override
+                public void run() {
+                    //prevent deadlock on shutdown
+                    frmMainCopy.dispose();
+                }
+            }.start();
             frmMain = null;
         }
     }
