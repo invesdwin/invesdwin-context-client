55830e4d56f6ee6eaee84857e7814a19f244d741
subes <gsubes@gmail.com>
2020-04-21 21:14:42 +0200

    better error handling

--- invesdwin-context-client-parent/invesdwin-context-client-swing/src/main/java/de/invesdwin/context/client/swing/error/GuiExceptionHandler.java
+++ invesdwin-context-client-parent/invesdwin-context-client-swing/src/main/java/de/invesdwin/context/client/swing/error/GuiExceptionHandler.java
@@ -1,10 +1,13 @@
 package de.invesdwin.context.client.swing.error;
 
 import java.awt.Dimension;
+import java.awt.Window;
 import java.awt.event.WindowEvent;
 import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.annotation.concurrent.ThreadSafe;
@@ -23,6 +26,7 @@ import de.invesdwin.context.log.error.LoggedRuntimeException;
 import de.invesdwin.context.log.error.hook.ErrHookManager;
 import de.invesdwin.context.log.error.hook.IErrHook;
 import de.invesdwin.util.assertions.Assertions;
+import de.invesdwin.util.concurrent.future.ImmutableFuture;
 import de.invesdwin.util.error.Throwables;
 import de.invesdwin.util.shutdown.ShutdownHookManager;
 import de.invesdwin.util.swing.Components;
@@ -55,54 +59,78 @@ public final class GuiExceptionHandler implements IErrHook {
     @Override
     public void loggedException(final LoggedRuntimeException exc, final boolean uncaughtException) {
         if (uncaughtException && !ShutdownHookManager.isShuttingDown()) {
+            handleException(exc, false);
+        }
+    }
+
+    public Future<Void> handleException(final LoggedRuntimeException exc, final boolean forced) {
+        if (!forced) {
             for (final IGuiExceptionHandlerHook hook : hooks) {
                 if (hook.shouldHideException(exc)) {
-                    return;
+                    return ImmutableFuture.of(null);
                 }
             }
-            final ResourceMap resourceMap = DelegateRichApplication.getInstance()
-                    .getContext()
-                    .getResourceMap(this.getClass());
-            final String title = resourceMap.getString("errorInfo.title");
-            final StringBuilder basicErrorMessage = new StringBuilder("<html>");
-            basicErrorMessage.append(resourceMap.getString("errorInfo.text"));
-            basicErrorMessage.append("<br>");
-            basicErrorMessage.append("<br><b>");
-            basicErrorMessage.append(Components.getDefaultToolTipFormatter()
-                    .format(Throwables.concatMessages(exc).replace("\n", "<br>")));
-            basicErrorMessage.append("</b>");
+        }
+        final ResourceMap resourceMap = getResourceMap();
+        final String title = GuiService.i18n(resourceMap, "errorInfo.title", "Unexpected Error");
+        final StringBuilder basicErrorMessage = new StringBuilder("<html>");
+        basicErrorMessage.append(
+                GuiService.i18n(resourceMap, "errorInfo.text", "An unexpected error occured. Please contact support."));
+        basicErrorMessage.append("<br>");
+        basicErrorMessage.append("<br><b>");
+        basicErrorMessage.append(
+                Components.getDefaultToolTipFormatter().format(Throwables.concatMessages(exc).replace("\n", "<br>")));
+        basicErrorMessage.append("</b>");
 
-            /*
-             * prevent the window manager from overflowing due to endless bombardment with error dialogs
-             */
-            if (ALREADY_SHOWING_COUNT.incrementAndGet() <= MAX_EXCEPTIONS_SHOWING) {
-                EventDispatchThreadUtil.invokeLater(new Runnable() {
-                    @Override
-                    public void run() {
-                        final ErrorInfo info = new ErrorInfo(title, basicErrorMessage.toString(), null, null, exc, null,
-                                null);
-                        final JXErrorPane pane = new JXErrorPane();
-                        pane.setErrorInfo(info);
-                        final JDialog dialog = JXErrorPane.createDialog(GuiService.get().getWindow(), pane);
-                        dialog.setTitle("Error");
-                        dialog.setSize(new Dimension(800, dialog.getHeight()));
-                        dialog.setMinimumSize(new Dimension(100, 100));
-                        dialog.addWindowListener(new WindowListenerSupport() {
-                            @Override
-                            public void windowClosed(final WindowEvent e) {
-                                ALREADY_SHOWING_COUNT.decrementAndGet();
-                                if (Err.isSameMeaning(shutdownAfterShowing, exc)) {
-                                    System.exit(1);
-                                }
+        /*
+         * prevent the window manager from overflowing due to endless bombardment with error dialogs
+         */
+        if (ALREADY_SHOWING_COUNT.incrementAndGet() <= MAX_EXCEPTIONS_SHOWING) {
+            return EventDispatchThreadUtil.invokeLater(new Callable<Void>() {
+                @Override
+                public Void call() {
+                    final ErrorInfo info = new ErrorInfo(title, basicErrorMessage.toString(), null, null, exc, null,
+                            null);
+                    final JXErrorPane pane = new JXErrorPane();
+                    pane.setErrorInfo(info);
+                    final JDialog dialog = JXErrorPane.createDialog(getWindow(), pane);
+                    dialog.setTitle("Error");
+                    dialog.setSize(new Dimension(800, dialog.getHeight()));
+                    dialog.setMinimumSize(new Dimension(100, 100));
+                    dialog.addWindowListener(new WindowListenerSupport() {
+                        @Override
+                        public void windowClosed(final WindowEvent e) {
+                            ALREADY_SHOWING_COUNT.decrementAndGet();
+                            if (Err.isSameMeaning(shutdownAfterShowing, exc)) {
+                                System.exit(1);
                             }
-                        });
-                        Dialogs.installEscapeCloseOperation(dialog);
-                        dialog.setVisible(true);
-                    }
-                });
-            } else {
-                ALREADY_SHOWING_COUNT.decrementAndGet();
-            }
+                        }
+                    });
+                    Dialogs.installEscapeCloseOperation(dialog);
+                    dialog.setVisible(true);
+                    return null;
+                }
+
+            });
+        } else {
+            ALREADY_SHOWING_COUNT.decrementAndGet();
+        }
+        return ImmutableFuture.of(null);
+    }
+
+    private ResourceMap getResourceMap() {
+        try {
+            return DelegateRichApplication.getInstance().getContext().getResourceMap(this.getClass());
+        } catch (final Throwable t) {
+            return null;
+        }
+    }
+
+    private Window getWindow() {
+        try {
+            return GuiService.get().getWindow();
+        } catch (final Throwable t) {
+            return Dialogs.getRootFrame();
         }
     }
 }
