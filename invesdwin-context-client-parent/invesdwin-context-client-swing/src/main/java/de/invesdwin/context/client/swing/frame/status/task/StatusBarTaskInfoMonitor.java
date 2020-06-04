c1958209c0e7f9173f3e7b7cdc26c8c8a63efc10
subes <gsubes@gmail.com>
2020-06-04 20:19:54 +0200

    better handle lots of tasks

--- invesdwin-context-client-parent/invesdwin-context-client-swing/src/main/java/de/invesdwin/context/client/swing/frame/status/task/StatusBarTaskInfoMonitor.java
+++ invesdwin-context-client-parent/invesdwin-context-client-swing/src/main/java/de/invesdwin/context/client/swing/frame/status/task/StatusBarTaskInfoMonitor.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import javax.annotation.concurrent.ThreadSafe;
 import javax.inject.Inject;
 
+import org.jdesktop.application.Application;
 import org.jdesktop.application.TaskService;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
@@ -51,71 +52,88 @@ public class StatusBarTaskInfoMonitor implements IRichApplicationHook, ITaskInfo
     }
 
     @Override
-    public synchronized void onTaskInfoAdded(final String name) {
-        if (!name_task.containsKey(name)) {
-            final ATask<Object, Object> task = new ATask<Object, Object>(DelegateRichApplication.getInstance()) {
-
-                {
-                    setTitle(name);
-                }
-
-                @Override
-                protected Object doInBackground() throws Exception {
-                    while (!Threads.isInterrupted()) {
-                        final TaskInfo taskInfo = TaskInfoManager.getTaskInfo(name);
-                        if (taskInfo == null) {
-                            break;
-                        }
-                        updateProgress(taskInfo);
-                        updateMessage(taskInfo);
-                        updateDescription(taskInfo);
-                        CHECK_INTERVAL.sleep();
-                    }
-                    return null;
-                }
-
-                private void updateProgress(final TaskInfo taskInfo) {
-                    final Percent progress = taskInfo.getProgress();
-                    if (progress != null) {
-                        final int percentage = (int) progress.getValue(PercentScale.PERCENT);
-                        if (percentage < 0 || percentage > 100) {
-                            setProgress(0);
-                        } else {
-                            setProgress(percentage);
-                        }
-                    } else {
-                        setProgress(0);
-                    }
-                }
-
-                private void updateMessage(final TaskInfo taskInfo) {
-                    if (taskInfo.getTasksCount() > 1) {
-                        setMessage(taskInfo.getCompletedCount() + "/" + taskInfo.getTasksCount());
-                    } else {
-                        setMessage(null);
-                    }
-                }
-
-                private void updateDescription(final TaskInfo taskInfo) {
-                    final Set<String> descriptions = taskInfo.getDescriptions();
-                    if (!descriptions.isEmpty()) {
-                        setDescription(Strings.escapeHtml4(descriptions.iterator().next()));
-                    } else {
-                        setDescription(null);
-                    }
-                }
-
-            };
-            name_task.put(name, task);
-            taskService.execute(task);
+    public void onTaskInfoAdded(final String name) {
+        synchronized (name_task) {
+            if (!name_task.containsKey(name)) {
+                final ATask<Object, Object> task = new TaskInfoTask(DelegateRichApplication.getInstance(), name);
+                name_task.put(name, task);
+                taskService.execute(task);
+            }
         }
     }
 
     @Override
-    public synchronized void onTaskInfoRemoved(final String name) {
-        final ATask<?, ?> task = name_task.remove(name);
-        if (task != null) {
-            task.cancel(true);
+    public void onTaskInfoRemoved(final String name) {
+    }
+
+    private final class TaskInfoTask extends ATask<Object, Object> {
+
+        private final String name;
+
+        private TaskInfoTask(final Application application, final String name) {
+            super(application);
+            setTitle(name);
+            this.name = name;
+        }
+
+        @Override
+        protected Object doInBackground() throws Exception {
+            boolean removed = false;
+            try {
+                while (!Threads.isInterrupted()) {
+                    TaskInfo taskInfo = TaskInfoManager.getTaskInfo(name);
+                    if (taskInfo == null) {
+                        synchronized (name_task) {
+                            taskInfo = TaskInfoManager.getTaskInfo(name);
+                            if (taskInfo == null) {
+                                name_task.remove(name);
+                                removed = true;
+                                break;
+                            }
+                        }
+                    }
+                    updateProgress(taskInfo);
+                    updateMessage(taskInfo);
+                    updateDescription(taskInfo);
+                    CHECK_INTERVAL.sleep();
+                }
+                return null;
+            } finally {
+                if (!removed) {
+                    name_task.remove(name);
+                }
+            }
+        }
+
+        private void updateProgress(final TaskInfo taskInfo) {
+            final Percent progress = taskInfo.getProgress();
+            if (progress != null) {
+                final int percentage = (int) progress.getValue(PercentScale.PERCENT);
+                if (percentage < 0 || percentage > 100) {
+                    setProgress(0);
+                } else {
+                    setProgress(percentage);
+                }
+            } else {
+                setProgress(0);
+            }
+        }
+
+        private void updateMessage(final TaskInfo taskInfo) {
+            if (taskInfo.getTasksCount() > 1) {
+                setMessage(taskInfo.getCompletedCount() + "/" + taskInfo.getTasksCount());
+            } else {
+                setMessage(null);
+            }
+        }
+
+        private void updateDescription(final TaskInfo taskInfo) {
+            final Set<String> descriptions = taskInfo.getDescriptions();
+            if (!descriptions.isEmpty()) {
+                setDescription(Strings.escapeHtml4(descriptions.iterator().next()));
+            } else {
+                setDescription(null);
+            }
         }
     }
 
