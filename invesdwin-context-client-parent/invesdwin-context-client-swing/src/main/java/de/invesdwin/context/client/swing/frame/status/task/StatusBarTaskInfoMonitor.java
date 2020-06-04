d621d2b007bc6b68f9776c49caf4f97fd62d0aae
subes <gsubes@gmail.com>
2020-06-04 20:04:09 +0200

    prevent taskinfo from overloading

--- invesdwin-context-client-parent/invesdwin-context-client-swing/src/main/java/de/invesdwin/context/client/swing/frame/status/task/StatusBarTaskInfoMonitor.java
+++ invesdwin-context-client-parent/invesdwin-context-client-swing/src/main/java/de/invesdwin/context/client/swing/frame/status/task/StatusBarTaskInfoMonitor.java
@@ -51,62 +51,64 @@ public class StatusBarTaskInfoMonitor implements IRichApplicationHook, ITaskInfo
     }
 
     @Override
-    public void onTaskInfoAdded(final String name) {
-        final ATask<Object, Object> task = new ATask<Object, Object>(DelegateRichApplication.getInstance()) {
+    public synchronized void onTaskInfoAdded(final String name) {
+        if (!name_task.containsKey(name)) {
+            final ATask<Object, Object> task = new ATask<Object, Object>(DelegateRichApplication.getInstance()) {
 
-            {
-                setTitle(name);
-            }
-
-            @Override
-            protected Object doInBackground() throws Exception {
-                while (!Threads.isInterrupted()) {
-                    final TaskInfo taskInfo = TaskInfoManager.getTaskInfo(name);
-                    if (taskInfo == null) {
-                        break;
-                    }
-                    updateProgress(taskInfo);
-                    updateMessage(taskInfo);
-                    updateDescription(taskInfo);
-                    CHECK_INTERVAL.sleep();
+                {
+                    setTitle(name);
                 }
-                return null;
-            }
 
-            private void updateProgress(final TaskInfo taskInfo) {
-                final Percent progress = taskInfo.getProgress();
-                if (progress != null) {
-                    final int percentage = (int) progress.getValue(PercentScale.PERCENT);
-                    if (percentage < 0 || percentage > 100) {
-                        setProgress(0);
+                @Override
+                protected Object doInBackground() throws Exception {
+                    while (!Threads.isInterrupted()) {
+                        final TaskInfo taskInfo = TaskInfoManager.getTaskInfo(name);
+                        if (taskInfo == null) {
+                            break;
+                        }
+                        updateProgress(taskInfo);
+                        updateMessage(taskInfo);
+                        updateDescription(taskInfo);
+                        CHECK_INTERVAL.sleep();
+                    }
+                    return null;
+                }
+
+                private void updateProgress(final TaskInfo taskInfo) {
+                    final Percent progress = taskInfo.getProgress();
+                    if (progress != null) {
+                        final int percentage = (int) progress.getValue(PercentScale.PERCENT);
+                        if (percentage < 0 || percentage > 100) {
+                            setProgress(0);
+                        } else {
+                            setProgress(percentage);
+                        }
                     } else {
-                        setProgress(percentage);
+                        setProgress(0);
                     }
-                } else {
-                    setProgress(0);
                 }
-            }
 
-            private void updateMessage(final TaskInfo taskInfo) {
-                if (taskInfo.getTasksCount() > 1) {
-                    setMessage(taskInfo.getCompletedCount() + "/" + taskInfo.getTasksCount());
-                } else {
-                    setMessage(null);
+                private void updateMessage(final TaskInfo taskInfo) {
+                    if (taskInfo.getTasksCount() > 1) {
+                        setMessage(taskInfo.getCompletedCount() + "/" + taskInfo.getTasksCount());
+                    } else {
+                        setMessage(null);
+                    }
                 }
-            }
 
-            private void updateDescription(final TaskInfo taskInfo) {
-                final Set<String> descriptions = taskInfo.getDescriptions();
-                if (!descriptions.isEmpty()) {
-                    setDescription(Strings.escapeHtml4(descriptions.iterator().next()));
-                } else {
-                    setDescription(null);
+                private void updateDescription(final TaskInfo taskInfo) {
+                    final Set<String> descriptions = taskInfo.getDescriptions();
+                    if (!descriptions.isEmpty()) {
+                        setDescription(Strings.escapeHtml4(descriptions.iterator().next()));
+                    } else {
+                        setDescription(null);
+                    }
                 }
-            }
 
-        };
-        name_task.put(name, task);
-        taskService.execute(task);
+            };
+            name_task.put(name, task);
+            taskService.execute(task);
+        }
     }
 
     @Override
