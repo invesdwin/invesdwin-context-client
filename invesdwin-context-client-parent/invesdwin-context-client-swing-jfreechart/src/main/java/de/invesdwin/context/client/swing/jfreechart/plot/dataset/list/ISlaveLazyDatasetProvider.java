d9c0996530b062674e1ba9fa906a6168bb80817a
subes <gsubes@gmail.com>
2020-08-20 21:32:33 +0200

    functional interface

--- invesdwin-context-client-parent/invesdwin-context-client-swing-jfreechart/src/main/java/de/invesdwin/context/client/swing/jfreechart/plot/dataset/list/ISlaveLazyDatasetProvider.java
+++ invesdwin-context-client-parent/invesdwin-context-client-swing-jfreechart/src/main/java/de/invesdwin/context/client/swing/jfreechart/plot/dataset/list/ISlaveLazyDatasetProvider.java
@@ -3,6 +3,7 @@ package de.invesdwin.context.client.swing.jfreechart.plot.dataset.list;
 import de.invesdwin.context.jfreechart.dataset.TimeRangedOHLCDataItem;
 import de.invesdwin.util.time.fdate.FDate;
 
+@FunctionalInterface
 public interface ISlaveLazyDatasetProvider {
 
     TimeRangedOHLCDataItem getValue(FDate key);
