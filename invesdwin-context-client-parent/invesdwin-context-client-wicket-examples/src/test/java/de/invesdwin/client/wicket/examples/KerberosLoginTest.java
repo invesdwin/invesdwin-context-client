7299dc1a13a84049bcc919b865c2f2078d0fcbb2
subes <gsubes@gmail.com>
2020-10-24 19:40:56 +0200

    another kerberos test

--- invesdwin-context-client-parent/invesdwin-context-client-wicket-examples/src/test/java/de/invesdwin/client/wicket/examples/KerberosLoginTest.java
+++ invesdwin-context-client-parent/invesdwin-context-client-wicket-examples/src/test/java/de/invesdwin/client/wicket/examples/KerberosLoginTest.java
@@ -1,7 +1,11 @@
 package de.invesdwin.client.wicket.examples;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,6 +15,20 @@ import javax.security.auth.Subject;
 import javax.security.auth.kerberos.KerberosPrincipal;
 import javax.security.auth.login.LoginContext;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.auth.AuthSchemeProvider;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.client.config.AuthSchemes;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.config.Lookup;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.impl.auth.SPNegoSchemeFactory;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.core.io.FileSystemResource;
@@ -25,6 +43,7 @@ import de.invesdwin.context.security.ldap.directory.server.test.DirectoryServerT
 import de.invesdwin.context.test.ATest;
 import de.invesdwin.context.webserver.test.WebserverTest;
 import de.invesdwin.util.assertions.Assertions;
+import de.invesdwin.util.lang.Files;
 
 @WebserverTest
 @NotThreadSafe
@@ -60,20 +79,6 @@ public class KerberosLoginTest extends ATest {
         loginContext.logout();
     }
 
-    @Test
-    @Ignore("does not seem to work properly with apacheds; works fine with MIT Kerberos")
-    public void testKerberosRestTemplate() throws InterruptedException {
-        //with keytab
-        final String responseFromKeytab = new ProxyEnabledKerberosRestTemplate(keytab, PRINCIPAL).getForObject(
-                IntegrationProperties.WEBSERVER_BIND_URI + "/" + SpringKerberosSecurePage.MOUNT_PATH, String.class);
-        Assertions.assertThat(responseFromKeytab).contains("Kerberos Authentication");
-
-        //from ticket cache
-        final String responseFromTicketCache = new ProxyEnabledKerberosRestTemplate().getForObject(
-                IntegrationProperties.WEBSERVER_BIND_URI + "/" + SpringKerberosSecurePage.MOUNT_PATH, String.class);
-        Assertions.assertThat(responseFromTicketCache).contains("Kerberos Authentication");
-    }
-
     private SunJaasKrb5LoginConfig createClientConfig(final String servicePrincipal, final File keytab)
             throws Exception {
         final SunJaasKrb5LoginConfig config = createConfig(servicePrincipal, keytab);
@@ -89,4 +94,90 @@ public class KerberosLoginTest extends ATest {
         config.afterPropertiesSet();
         return config;
     }
+
+    @Test
+    @Ignore("does not seem to work properly with apacheds; works fine with MIT Kerberos")
+    public void testKerberosRestTemplate() throws Exception {
+        //with keytab
+        final String responseFromKeytab = new ProxyEnabledKerberosRestTemplate(keytab, PRINCIPAL).getForObject(
+                IntegrationProperties.WEBSERVER_BIND_URI + "/" + SpringKerberosSecurePage.MOUNT_PATH, String.class);
+        Assertions.assertThat(responseFromKeytab).contains("Kerberos Authentication");
+
+        //from ticket cache
+        final String responseFromTicketCache = new ProxyEnabledKerberosRestTemplate().getForObject(
+                IntegrationProperties.WEBSERVER_BIND_URI + "/" + SpringKerberosSecurePage.MOUNT_PATH, String.class);
+        Assertions.assertThat(responseFromTicketCache).contains("Kerberos Authentication");
+    }
+
+    /**
+     * https://stackoverflow.com/questions/24633380/how-do-i-authenticate-with-spnego-kerberos-and-apaches-httpclient
+     */
+    @SuppressWarnings("deprecation")
+    @Test
+    @Ignore("does not seem to work")
+    public void testKerberosHttpClient() throws Exception {
+        // Depending on your AD configuration, this file is not needed
+        final File krb5Config = createKrb5Configuration("INVESDWIN.DE",
+                IntegrationProperties.WEBSERVER_BIND_URI.getHost());
+        final String url = IntegrationProperties.WEBSERVER_BIND_URI + "/" + SpringKerberosSecurePage.MOUNT_PATH;
+
+        //CHECKSTYLE:OFF
+        System.out.println("Created config '" + krb5Config.getAbsolutePath() + "':");
+        System.out.println(Files.readFileToString(krb5Config, Charset.defaultCharset()));
+
+        System.setProperty("java.security.krb5.conf", krb5Config.toURI().toString());
+        System.setProperty("sun.security.krb5.debug", "true");
+        System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
+        //CHECKSTYLE:ON
+
+        final Lookup<AuthSchemeProvider> authSchemeRegistry = RegistryBuilder.<AuthSchemeProvider> create()
+                .register(AuthSchemes.SPNEGO, new SPNegoSchemeFactory(true))
+                .build();
+
+        final CloseableHttpClient client = HttpClients.custom()
+                .setDefaultAuthSchemeRegistry(authSchemeRegistry)
+                .build();
+        final HttpClientContext context = HttpClientContext.create();
+        final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+
+        // This may seem odd, but specifying 'null' as principal tells java to use the logged in user's credentials
+        final Credentials useJaasCreds = new Credentials() {
+
+            @Override
+            public String getPassword() {
+                return null;
+            }
+
+            @Override
+            public Principal getUserPrincipal() {
+                return null;
+            }
+
+        };
+        credentialsProvider.setCredentials(new AuthScope(null, -1, null), useJaasCreds);
+        context.setCredentialsProvider(credentialsProvider);
+
+        final HttpGet httpget = new HttpGet(url);
+        final CloseableHttpResponse response = client.execute(httpget, context);
+        final String responseString = IOUtils.toString(response.getEntity().getContent());
+        Assertions.assertThat(responseString).contains("Kerberos Authentication");
+    }
+
+    @SuppressWarnings("deprecation")
+    private static File createKrb5Configuration(final String domain, final String kdc) throws IOException {
+        final File tempFile = File.createTempFile("krb5", "kdc");
+        final ArrayList<String> lines = new ArrayList<>();
+        lines.add("[libdefaults]");
+        lines.add("\tdefault_realm = " + domain);
+        lines.add("[realms]");
+        lines.add("\t" + domain + " = {");
+        lines.add("\t\tkdc = " + kdc);
+        lines.add("\t\tadmin_server = " + kdc);
+        lines.add("\t}");
+        final FileWriter writer = new FileWriter(tempFile);
+        IOUtils.writeLines(lines, System.lineSeparator(), writer);
+        IOUtils.closeQuietly(writer);
+        return tempFile;
+    }
+
 }
