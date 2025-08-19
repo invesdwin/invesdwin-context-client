3d257f302a37eebfe2fae902ef511c808d85af44
subes <gsubes@gmail.com>
2025-08-19 23:50:50 +0200

    PER-960 state fix

--- invesdwin-context-client-parent/invesdwin-context-client-swing/src/main/java/de/invesdwin/context/client/swing/api/view/AView.java
+++ invesdwin-context-client-parent/invesdwin-context-client-swing/src/main/java/de/invesdwin/context/client/swing/api/view/AView.java
@@ -217,13 +217,14 @@ public abstract class AView<M extends AModel, C extends JComponent> extends AMod
     public void replaceView(final AView<?, ?> existingView) {
         synchronized (dockableLock) {
             synchronized (existingView.dockableLock) {
-                Assertions.assertThat(existingView.dockable).isNotNull();
                 //move dockable
                 this.dockable = existingView.dockable;
                 existingView.dockable = null;
                 //replace dockable content
-                this.dockable.setView(this);
-                this.dockable.setComponent(getComponent());
+                if (this.dockable != null) {
+                    this.dockable.setView(this);
+                    this.dockable.setComponent(getComponent());
+                }
                 //close existing view
                 new AViewVisitor() {
                     @Override
