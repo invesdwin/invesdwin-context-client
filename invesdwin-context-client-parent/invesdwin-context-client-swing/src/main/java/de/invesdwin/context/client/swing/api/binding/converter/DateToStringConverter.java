274ae1d4086864847b39534c4983fcc1375a3cac
Edwin Stang <gsubes@gmail.com>
2026-05-28 15:05:08 +0200

    PER-1067 switch from _MS to _PS date formats everywhere (since that is now the default FDate.toString() format); add more types to FDate converters

--- invesdwin-context-client-parent/invesdwin-context-client-swing/src/main/java/de/invesdwin/context/client/swing/api/binding/converter/DateToStringConverter.java
+++ invesdwin-context-client-parent/invesdwin-context-client-swing/src/main/java/de/invesdwin/context/client/swing/api/binding/converter/DateToStringConverter.java
@@ -1,28 +1,26 @@
 package de.invesdwin.context.client.swing.api.binding.converter;
 
 import java.text.ParseException;
-import java.util.Calendar;
-import java.util.Date;
 
 import javax.annotation.concurrent.Immutable;
 
 import de.invesdwin.norva.beanpath.spi.element.IPropertyBeanPathElement;
-import de.invesdwin.util.error.UnknownArgumentException;
 import de.invesdwin.util.lang.string.Strings;
 import de.invesdwin.util.time.date.FDate;
+import de.invesdwin.util.time.date.format.FDateTimeFormatter;
 
 @Immutable
 public class DateToStringConverter implements IConverter<Object, String> {
 
-    private final java.text.SimpleDateFormat format;
+    private final FDateTimeFormatter format;
     private final Class<?> type;
 
     public DateToStringConverter(final IPropertyBeanPathElement element) {
         final String formatStr = element.getFormatString();
         if (formatStr != null) {
-            format = new java.text.SimpleDateFormat(formatStr);
+            format = FDateTimeFormatter.forPattern(formatStr);
         } else {
-            format = new java.text.SimpleDateFormat(FDate.FORMAT_ISO_DATE_TIME_MS);
+            format = FDateTimeFormatter.forPattern(FDate.FORMAT_ISO_DATE_TIME_PS);
         }
         this.type = element.getModifier().getBeanClassAccessor().getType().getType();
     }
@@ -32,19 +30,7 @@ public class DateToStringConverter implements IConverter<Object, String> {
         if (value == null) {
             return null;
         }
-        final Date date;
-        if (value instanceof Date) {
-            date = (Date) value;
-        } else if (value instanceof Calendar) {
-            final Calendar cValue = (Calendar) value;
-            date = cValue.getTime();
-        } else if (value instanceof FDate) {
-            final FDate cValue = (FDate) value;
-            date = cValue.dateValue();
-        } else {
-            throw UnknownArgumentException.newInstance(Class.class, value.getClass());
-        }
-        return format.format(date);
+        return format.print(value);
     }
 
     @Override
@@ -52,15 +38,6 @@ public class DateToStringConverter implements IConverter<Object, String> {
         if (Strings.isBlank(value)) {
             return null;
         }
-        final Date date = format.parse(value);
-        if (type == Date.class) {
-            return date;
-        } else if (type == Calendar.class) {
-            return new FDate(date).calendarValue();
-        } else if (type == FDate.class) {
-            return new FDate(date);
-        } else {
-            throw UnknownArgumentException.newInstance(Class.class, type);
-        }
+        return format.parse(type, value);
     }
 }
